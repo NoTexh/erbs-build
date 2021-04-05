e9826921172aff430ccc233b6ebadbd1655b69ed
Tobi <BlackDeath@landing-zone.de>
2021-04-06 01:01:59 +0200

    added axe data

---
+++ src/models/axes.js
@@ -0,0 +1,15 @@
+const axes = [
+    {
+        "name": "Chain Scythe",
+        "rarity": "green",
+        "attack_power": 50,
+        "attack_range": 0.2
+    },
+    {
+        "name": "Battle Axe",
+        "rarity": "green",
+        "attack_power": 47
+    }
+]
+
+export default axes;

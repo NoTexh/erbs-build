ed364b3a9454951b30b2d2c2d06f7b9fce9c4e39
Tobi <BlackDeath@landing-zone.de>
2021-04-14 19:51:11 +0200

    wip: name changes

--- src/components/Catalog/LocationField.vue
+++ src/components/Catalog/LocationField.vue
@@ -3,16 +3,11 @@
     <h1>{{ locationName }}</h1>
 
     <ul>
-      <li v-for="item in locationItems" v-bind:key="item.id">
-        {{ item.itemName.name }} {{ item.amount }}
+      <li v-for="locationItem in locationItems" v-bind:key="locationItem.id">
+        {{ locationItem.item.name }} {{ locationItem.amount }}
       </li>
     </ul>
 
-    <!--
-    <ul>
-      <li v-for="{itemName, amount, id} in locationItems" :key="id">{{ itemName }} {{ amount }}</li>
-    </ul>
-    -->
   </div>
 </template>
 
